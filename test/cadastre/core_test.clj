c7fada3f84e639bd7b4eac9e6d108fdfbdafe47a
Michael Klishin <michael@clojurewerkz.org>
2013-11-24 15:33:30 +0400

    Rename test ns

--- test/cadastre/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cadastre.core-test
-  (:use clojure.test
-        cadastre.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
---
+++ test/clojuredocs/extractor/analyzer_test.clj
@@ -0,0 +1,3 @@
+(ns clojuredocs.extractor.analyzer-test
+  (:require [clojuredocs.extractor.analyzer :as xa]
+            [clojure.test :refer :all]))
