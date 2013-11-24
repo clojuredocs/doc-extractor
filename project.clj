996b36e8180e77e27f4d11556413e5fc398121c3
Michael Klishin <michael@clojurewerkz.org>
2013-11-24 15:40:00 +0400

    Update project source URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojuredocs/extractor "0.2.0-SNAPSHOT"
   :description "Survey a clojure project and extract valuable metadata."
-  :url "https://github.com/dakrone/cadastre"
+  :url "https://github.com/clojuredocs/doc-extractor"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.4"]
