f49f43f0fa1932499af6cbba97a8fb2502a7ca5f
Michael Klishin <michael@clojurewerkz.org>
2013-11-24 15:35:36 +0400

    Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/tools.namespace "0.2.4"]
                  [cheshire "5.2.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
